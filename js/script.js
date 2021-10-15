4d58209e2ddb4de05e47c4a9865617c4b1b823a1
Ольга <91964782+Shysha007@users.noreply.github.com>
2021-10-16 01:35:57 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
-for (i = 100; i >= 1; i--) {
+// Task 2
+ for (let i = 100; i > 0; i--) {
+  console.log(i);
+ } //от 100 до 1 включительно.
+//Task 3 
+for (let i = 2; i <= 100; i += 2) {
     console.log(i);
-} //от 100 до 1 включительно.
+}
+// Task 4
+const arr = [1, 2, 3, 4, 5];
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+	sum += arr[i]
+}
+  console.log(sum);
+// Task 5
+const num = [1, 2, 3, 4, 5];
+let result = 0;
+for (let i = 0; i < arr.length; i++) {
+    result +=  Math.pow(num[i], 2);
+}
+ console.log(result);
